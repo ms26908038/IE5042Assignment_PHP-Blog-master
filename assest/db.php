ab24136eebecb0b0e2b0c3a55f274cdaec8ef2e3
Kushan Madushanka <ms26908038@my.sliit.lk>
2026-03-21 19:23:33 +0530

    Set the password to DB

--- assest/db.php
+++ assest/db.php
@@ -22,7 +22,7 @@
     server with default setting (user 'root' with no password) */
     define('DB_SERVER', 'localhost');
     define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '');
+    define('DB_PASSWORD', '1234');
     define('DB_NAME', 'blog');
 
     /* Attempt to connect to MySQL database */
