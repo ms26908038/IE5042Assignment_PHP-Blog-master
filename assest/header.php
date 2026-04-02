1529a86058d9ce10efbb946d2bc140c8bc6bb1d2
Kushan Madushanka <ms26908038@my.sliit.lk>
2026-04-02 15:53:15 +0530

    Remove Default Login Button from Header

--- assest/header.php
+++ assest/header.php
@@ -1,3 +1,4 @@
+<?php /* -- Remove Defult Login Button from Header--*/ ?>
 <header class="blog-header border-bottom shadow-sm bg-white">
     <div class="container-fluid" style="padding-left: 3rem; padding-right:3rem">
 
@@ -7,24 +8,21 @@
             </a>
 
             <?php if ($loggedin) : ?>
-
                 <nav class="my-2 my-md-0 mr-md-3">
                     <a class="p-2 px-5 text-muted" href="index.php">Home</a>
                     <a class="p-2 px-5 text-muted" href="categories.php">Category</a>
                     <a class="p-2 px-5 text-muted" href="article.php">Article</a>
                     <a class="p-2 px-5 text-muted" href="author.php">Author</a>
                 </nav>
-
             <?php else : ?>
                 <nav class="my-2 my-md-0 mr-md-3">
                     <a class="p-2 px-5 text-muted" href="articleOfCategory.php">Articles</a>
                 </nav>
+            <?php endif; ?>
 
-            <?php endif;  ?>
-
-            <a class="btn btn-outline-success" href="<?= ($loggedin) ? 'Logout.php' : 'login.php'; ?>">
-                <?= ($loggedin) ? 'Logout' : 'Sign in'; ?>
-            </a>
+            <?php if ($loggedin) : ?>
+                <a class="btn btn-outline-danger" href="Logout.php">Logout</a>
+            <?php endif; ?>
 
         </div>
     </div>
