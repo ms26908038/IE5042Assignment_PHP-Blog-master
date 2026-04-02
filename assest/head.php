fa9c6d20febad9f9e1d3cc49bd0430c8fc8ae1b1
Kushan Madushanka <ms26908038@my.sliit.lk>
2026-04-02 15:48:38 +0530

    No chengers

--- assest/head.php
+++ assest/head.php
@@ -1,6 +1,7 @@
 <?php require "db.php"; ?>
 
 <?php
+
 // Initialize the session
 session_start();
 $loggedin = false;
@@ -8,7 +9,7 @@ $loggedin = false;
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
    $loggedin = true;
-    
+
 }
 
 ?>
