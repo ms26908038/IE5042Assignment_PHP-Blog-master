dfd2d03401737ea601e1a3574a79a0ea13b333da
Kushan Madushanka <ms26908038@my.sliit.lk>
2026-04-03 20:39:53 +0530

    Removing a wrong configuration B03: Sensitive Information Disclosure (CWE-538) The .htaccess Fix

--- .htaccess.php
+++
@@ -1,8 +0,0 @@
-# Block access to any file/folder starting with a dot (like .git)
-RewriteRule "(^|/)." - [F]
-
-# Block direct access to db.php so people can't try to run it directly
-<Files "db.php">
-Order Allow,Deny
-Deny from All
-</Files>
