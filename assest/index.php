094df85333e6b748ae2270d74d6b7c1a46375fbc
Kushan Madushanka <ms26908038@my.sliit.lk>
2026-04-03 20:35:43 +0530

    ID: B09: Directory Browsing fixing using option C

---
+++ assest/index.php
@@ -0,0 +1,26 @@
+<?php
+// This file exists to prevent Directory Browsing
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>End of the Internet</title>
+    <style>
+        body { 
+            background-color: #000; 
+            color: #0f0; 
+            font-family: 'Courier New', Courier, monospace; 
+            display: flex; 
+            justify-content: center; 
+            align-items: center; 
+            height: 100vh; 
+            margin: 0; 
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>Why are you here?<br>You are at the End of the Internet.</h1>
+</body>
+</html>
